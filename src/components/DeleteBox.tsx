b65305bc617a6c141ffbf5a31afacf7a2e1c6a72
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-11 09:11:50 +0100

    "

--- src/components/DeleteBox.tsx
+++ src/components/DeleteBox.tsx
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 import { Button } from "@mantine/core";
 import { ThumbDown } from "tabler-icons-react";
+
+export default function DeleteBox() {
+  return <div>DeleteBox</div>;
+}
