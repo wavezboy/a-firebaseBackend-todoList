7396331467a7347b0b94a520297ada3a019c523b
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-11 09:41:01 +0100

    deleteBox done

--- src/components/DeleteBox.tsx
+++ src/components/DeleteBox.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import styled from "styled-components";
 import { Button } from "@mantine/core";
 import { ThumbDown } from "tabler-icons-react";
 
-export default function DeleteBox() {
-  return <div>DeleteBox</div>;
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 1.875rem;
+`;
+
+export default function DeleteBox({ uid }) {
+  return (
+    <Container>
+      <Button onClick={() => {}} variant="filled" color="red">
+        Del
+      </Button>
+    </Container>
+  );
 }
